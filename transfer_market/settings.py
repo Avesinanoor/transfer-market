cab776fb85b0abfc19ce3a00a82a619e9ec0ddf5
Daffa Abhinaya Avesinanoor <daffaabhi86@gmail.com>
2025-09-16 18:34:32 +0700

    edit settings.py

--- transfer_market/settings.py
+++ transfer_market/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "daffa-abhinaya-transfermarket.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://daffa-abhinaya-transfermarket.pbp.cs.ui.ac.id"
+    "https://daffa-abhinaya-transfermarket.pbp.cs.ui.ac.id"
 ]
 
 
